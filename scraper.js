0d7fa3d551c438019d8d0ef198bb62493b575f2f
Güven Can Yıldız <yildiz.guvencan@gmail.com>
2025-03-05 13:47:01 +0300

    Added: Free games

--- scraper.js
+++ scraper.js
@@ -3,8 +3,12 @@ const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-function writeToFile(data) {
+function appendToFile(data) {
   fs.appendFileSync('games_under_max_price.txt', data + '\n', 'utf8');
+}
+
+function writeToFile(text) {
+  fs.writeFileSync('games_under_max_price.txt', text);
 }
 
 const maxPrice = parseFloat(process.argv[2]) || 10.00;
@@ -18,49 +22,49 @@ async function fetchPage(page) {
     const $ = cheerio.load(response.data);
 
     $('a.games-list-item').each((index, element) => {
-      const priceText = $(element).find('.price-tag').contents().filter(function() {
-        return this.nodeType === 3; 
+      const priceText = $(element).find('.price-tag').contents().filter(function () {
+        return this.nodeType === 3;
       }).text().trim().replace('$', '').replace(',', '');
 
       const price = parseFloat(priceText);
-     
-      if (price <= maxPrice) {
+
+      if (priceText === "FREE" || price <= maxPrice) {
         const title = $(element).find('h5').text().trim();
-        const data = `Game: ${title}, Price: $${price}`;
-        writeToFile(data); 
+        const data = `Game: ${title}, Price: ${priceText === "FREE" ? priceText : "$" + price}`;
+        appendToFile(data);
       }
     });
   } catch (error) {
     if (error.response && error.response.status === 429) {
-        console.log(`Captcha Detected...`);
-        await tryBypassCaptcha(url);
-        return fetchPage(page); 
-      }
+      console.log(`Captcha Detected...`);
+      await tryBypassCaptcha(url);
+      return fetchPage(page);
+    }
 
-      console.error(error);
+    console.error(error);
   }
 }
 
 async function tryBypassCaptcha(url) {
-    console.log(`DO NOT CLOSE PAGE`);
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(url);
-    console.log('bypass CAPTCHA and close page...');
-    
-    await new Promise((resolve, reject) => {
-      const interval = setInterval(async () => {
-        const targets = await browser.targets();
-        const target = targets.find(t => t.type() === 'page');
-        if (!target) {
-          clearInterval(interval);
-          resolve();
-        }
-      }, 1000); 
-    });
-  
-    console.log('Browser closed...');
-  }
+  console.log(`DO NOT CLOSE PAGE`);
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+  console.log('bypass CAPTCHA and close page...');
+
+  await new Promise((resolve, reject) => {
+    const interval = setInterval(async () => {
+      const targets = await browser.targets();
+      const target = targets.find(t => t.type() === 'page');
+      if (!target) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 1000);
+  });
+
+  console.log('Browser closed...');
+}
 
 async function fetchAllPages() {
   for (let page = 1; page <= maxPages; page++) {
@@ -69,4 +73,5 @@ async function fetchAllPages() {
   }
 }
 
+writeToFile("");
 fetchAllPages();
